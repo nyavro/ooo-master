7893a5853fa24e4a682b74981fb64592926b109e
Nyavro <e.nyavro@gmail.com>
2015-09-17 23:42:01 +0600

    angular-ui-router test2

--- ui/app/assets/javascripts/app.js
+++ ui/app/assets/javascripts/app.js
@@ -1,20 +1,28 @@
 var routerApp = angular.module('routerApp', ['ui.router']);
 
-routerApp.config(function($stateProvider, $urlRouterProvider) {
-
-    $urlRouterProvider.otherwise('/home');
-
-    $stateProvider
-
+routerApp.config(
+    function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
-            url: '/home',
-            templateUrl: 'partial_home'
-        })
-
+                url: '/home',
+                templateUrl: 'partial_home'
+            }
+        )
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
-            // we'll get to this in a bit       
-        });
+            // we'll get to this in a bit
+            }
+        );
+    }
+).config([
+        '$controllerProvider', function($controllerProvider) {
+            $controllerProvider.allowGlobals();
+        }
+    ]
+);
 
-});
+function EmptyController($scope) {
+    $scope.message = "hello, man!";
+}
