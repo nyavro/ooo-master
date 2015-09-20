b8b0f61ca7e4f04e93177d787f60c24c812d061f
Nyavro <e.nyavro@gmail.com>
2015-09-20 11:06:08 +0600

    angular-ui-router working

--- ui/app/assets/javascripts/app.js
+++ ui/app/assets/javascripts/app.js
@@ -3,31 +3,30 @@ var dependencies = [
     'app.servicesModule',
     'app.controllers',
     'app.directives',
-    'app.common '
+    'app.common'
 ];
-var app = angular.module('app', dependencies);
-
-app.config(
-    function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/home');
-        $stateProvider
-            .state('home', {
-                    url: '/home',
-                    templateUrl: 'partial_home'
-                }
-            )
-            .state('about', {
-                    url: '/about',
-                    templateUrl: 'partial_about'
-                }
-            );
-    }
-).config([
-        '$controllerProvider', function($controllerProvider) {
-            $controllerProvider.allowGlobals();
+angular.module('app', dependencies)
+    .config(
+        function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/home');
+            $stateProvider
+                .state('home', {
+                        url: '/home',
+                        templateUrl: 'partial_home'
+                    }
+                )
+                .state('about', {
+                        url: '/about',
+                        templateUrl: 'partial_about'
+                    }
+                );
         }
-    ]
-);
+    ).config([
+            '$controllerProvider', function($controllerProvider) {
+                $controllerProvider.allowGlobals();
+            }
+        ]
+    );
 
 servicesModule = angular.module('app.servicesModule', []);
 controllersModule = angular.module('app.controllers', []);
